c6a933509b6a8d0218b8f9f2125a211d520797fb
danjperron <danjperron@videotron.ca>
2013-12-26 19:21:21 -0500

    add gyro offset calibration

--- AccTest.c
+++ AccTest.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
 #include <math.h>
 #include "I2CWrapper.h"
 #include "mpu6050.h"
-
+#include  <math.h>
 ////////////////////////////////////////////
 //
 //    program to test mpu6050 accelerometer
@@ -58,10 +60,49 @@ void sig_handler(int signo)
 }
 
 
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+
+
+
+
+
 int main(void)
 {
+   int loop;
    int i2c_handle;
-
+   float Gtotal;
+   float GSumSquare;
+   short GyroxOffset,GyroyOffset,GyrozOffset;
+   long  Xsum,Ysum,Zsum;
+   int CountSum;
+   const float  AccFactor=16.0 /32768.0;
+   const float  GyroFactor=500.0 / 32768.0;
    const BUS = 1;
    int I2C_Current_Slave_Adress=0x68;
 
@@ -80,22 +121,54 @@ int main(void)
        }
        else
       {
-           
+
            Setup_MPU6050(i2c_handle);
+
+           printf("Please be sure that the MPU6050 doesn't move and press space bar\n");
+           fflush(stdout);
+
+           while(!kbhit())
+             Get_Accel_Values(i2c_handle,&Data);
+           printf("calibrating Gyro..\n");
+           fflush(stdout);
+
+           Xsum=Ysum=Zsum=CountSum=0;
+           for(loop=0;loop<200;loop++)
+           {
+             while(!GotInt_MPU6050(i2c_handle));
+             Get_Accel_Values(i2c_handle,&Data);
+             Xsum += Data.Gyrox;
+             Ysum += Data.Gyroy;
+             Zsum += Data.Gyroz;
+             CountSum++;
+            }
+           GyroxOffset = Xsum / CountSum;
+           GyroyOffset = Ysum / CountSum;
+           GyrozOffset = Zsum / CountSum;
+     
            while(!ExitFlag)
            {
            usleep(100000); // wait a little
 
            Get_Accel_Values(i2c_handle,&Data);
-           printf("Gx=%3.1f Gy=%3.1f Gz=%3.1f  Temp=%3.1f\n",\
+           GSumSquare = ((float) Data.Gx) * Data.Gx;
+           GSumSquare += ((float) Data.Gy) * Data.Gy;
+           GSumSquare += ((float) Data.Gz) * Data.Gz;
+           Gtotal = sqrt(GSumSquare);
+          
+           printf("Accelerometer x=%+2.2f y=%+2.2f z=%+2.2f All=%2.2f Temp=%+3.1f Gyro x=%+3.2f y=%+3.2f z=%+3.2f\n",\
            //acceleration are in 20G scale
-           (float) Data.Gx * 16.0 / 32767,
-           (float) Data.Gy * 16.0 / 32767,
-           (float) Data.Gz * 16.0 / 32767,
+           AccFactor *  Data.Gx,
+           AccFactor *  Data.Gy,
+           AccFactor *  Data.Gz,
+           AccFactor *  Gtotal, 
            // temperature are 
            //Temperature in degrees C = (TEMP_OUT Register Value as a signed quantity)/340 + 36.53
 
-           (float)  Data.Temperature / 340.0 + 36.53);
+           (float)  Data.Temperature / 340.0 + 36.53,
+           GyroFactor * (Data.Gyrox - GyroxOffset),
+           GyroFactor * (Data.Gyroy - GyroyOffset),
+           GyroFactor * (Data.Gyroz - GyrozOffset));
           }
       }
 
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ mpu6050TestInC
 simple raspberry pi  program in C to test the mpu6050
 
 
- just do gcc -o AccTest AccTest.c mpu6050.c I2CWrapper.c
+ just do gcc -lm -o AccTest AccTest.c mpu6050.c I2CWrapper.c
  
  
