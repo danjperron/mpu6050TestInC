62d692b26979aacab225f3e289930851fed4f4e1
danjperron <danjperron@videotron.ca>
2014-01-18 06:04:03 +0000

    update for new MPU6050 module

--- VibrationTest.py
+++ VibrationTest.py
@@ -5,10 +5,11 @@ import math
 
 mpu6050 = MPU6050.MPU6050()
 
-mpu6050.Setup()
+mpu6050.setup()
+mpu6050.setSampleRate(100)
+mpu6050.setGResolution(16)
 #sometimes I need to reset twice
-I = mpu6050.ReadData()
-mpu6050.Setup()
+I = mpu6050.readData()
 
 
 
@@ -18,23 +19,23 @@ ShakeFlag = False;
 while True:
 
   #wait until new data available
-  while not(mpu6050.GotNewData()):
+  while (mpu6050.readStatus() & 1)==0 :
     pass
 
   #read initial reading and put in I class
-  I = mpu6050.ReadData()
+  I = mpu6050.readData()
 
 
   PeakForce = 0
 
   for loop in range(20):
 
-     #wait until data is available
-     while not(mpu6050.GotNewData()):
+     #wait until new data available
+     while (mpu6050.readStatus() & 1)==0 :
        pass
  
      # read the accelerometer
-     C = mpu6050.ReadData()
+     C = mpu6050.readData()
 
      #calculate new force
      CurrentForce = math.sqrt( (C.Gx - I.Gx) * ( C.Gx - I.Gx) + 
