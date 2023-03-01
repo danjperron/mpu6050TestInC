d8cdfaa7410e769ef742ee31c16e038b02bf3ce9
Daniel Perron <danjperron@videotron.ca>
2023-03-01 10:02:04 -0500

    Fix for python3

--- GetFFTmpu6050.py
+++ GetFFTmpu6050.py
@@ -8,8 +8,8 @@ import numpy
 #TargetSampleNumber= 1024
 #TargetRate =  33    # frequency =  8000 / ( integr value + 1)  minimum frequency=32,25
 
-InputSampleRate=raw_input("Sample Rate(32.25 ... 2000) ?")
-InputSampleNumber=raw_input("Number of sample to take ?")    
+InputSampleRate=input("Sample Rate(32.25 ... 2000) ?")
+InputSampleNumber=input("Number of sample to take ?")
 
 TargetSampleNumber= int(InputSampleNumber)
 TargetRate= float(InputSampleRate)
@@ -24,9 +24,9 @@ mpu6050.setSampleRate(TargetRate)
 mpu6050.enableFifo(False)
 time.sleep(0.01)
 
-print "Capturing {0} samples at {1} samples/sec".format(TargetSampleNumber, mpu6050.SampleRate)
+print("Capturing {0} samples at {1} samples/sec".format(TargetSampleNumber, mpu6050.SampleRate))
 
-raw_input("Press enter to start")
+input("Press enter to start")
 
 mpu6050.resetFifo()
 mpu6050.enableFifo(True)
@@ -43,12 +43,12 @@ while True:
 
      # print "Status",Status
      if (Status & 0x10) == 0x10 :
-        print "Overrun Error! Quitting.\n"
+        print("Overrun Error! Quitting.\n")
         quit()
 
      if (Status & 0x01) == 0x01:
         Values.extend(mpu6050.readDataFromFifo())
- 
+
  else:
         Values.extend(mpu6050.readDataFromFifo())
 
@@ -65,11 +65,11 @@ if Total > 0:
   Status = mpu6050.readStatus()
   # print "Status",Status
   if (Status & 0x10) == 0x10 :
-    print "Overrun Error! Quitting.\n"
+    print("Overrun Error! Quitting.\n")
     quit()
 
-  print "Saving RawData.txt  file."
-  
+  print("Saving RawData.txt  file.")
+
   FO = open("RawData.txt","w")
   FO.write("GT\tGx\tGy\tGz\tTemperature\tGyrox\tGyroy\tGyroz\n")
   fftdata = []
@@ -83,18 +83,18 @@ if Total > 0:
 
   FO.close()
 
-  print "Calculate FFT"
+  print("Calculate FFT")
 
   fourier = numpy.fft.fft(fftdata)
 
-  print "Save FFTData.txt file"
+  print("Save FFTData.txt file")
   FO = open("FFTData.txt","w")
-  fftData = numpy.abs(fourier[0:len(fourier)/2+1])/TargetSampleNumber
+  fftData = numpy.abs(fourier[0:len(fourier)//2+1])/TargetSampleNumber
   frequency = []
   FO.write( "Frequency\tFFT\n")
   Peak=0
   PeakIndex=0;
-  for loop in range(TargetSampleNumber/2 +1):
+  for loop in range(TargetSampleNumber//2 +1):
     frequency.append( loop * TargetRate/ TargetSampleNumber)
     FO.write("{0}\t{1}\n".format(frequency[loop],fftData[loop]))
     if loop>0:
@@ -102,7 +102,7 @@ if Total > 0:
          Peak=fftData[loop]
          PeakIndex=loop
 
-  print "Peak at {0}Hz = {1}".format(frequency[PeakIndex],Peak)
-   
+  print("Peak at {0}Hz = {1}".format(frequency[PeakIndex],Peak))
 
-print "Done!"
+
+print("Done!")
